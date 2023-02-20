0c61edafdf3eb39a6b3e21d9a3761357506be106
valeiras <d.r.valeiras@gmail.com>
2023-02-20 11:59:54 +0100

    Extra tools for calibration and control

---
+++ Tools/ServoCalibration/ServoCalibration.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+#define SERVO_PIN 6
+
+Servo servo1;
+
+void setup() {
+  servo1.attach(SERVO_PIN);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  servo1.write(90);
+  delay(1000);
+  servo1.write(90);
+  delay(1000);
+}
