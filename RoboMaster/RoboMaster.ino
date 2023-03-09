d205aab2f9c741e47a22bacb7a2d2bf036cc9e79
valeiras <d.r.valeiras@gmail.com>
2023-03-09 17:43:23 +0100

    Use MIDI input, instead of buttons and potentiometers

--- RoboMaster/RoboMaster.ino
+++ RoboMaster/RoboMaster.ino
@@ -1,13 +1,18 @@
 #include <Wire.h>
+#include <MIDI.h>
 #include <robo_communication.h>
 
-#define BPM_INPUT_PIN A0
-#define BUTTON1_PIN 2
+#include "midi_notes.h"
 
-#define ANALOG_MIN 0
-#define ANALOG_MAX 1023
+#define CC_MIN 0
+#define CC_MAX 127
+
+#define LED_PIN 13
 
 #define MIN_BPM_CHANGE 5
+
+// Create and bind the MIDI interface to the default hardware Serial port
+MIDI_CREATE_DEFAULT_INSTANCE();
 
 const uint16_t RESYNC_TIME = 10000;
 uint8_t bpm = 90;
@@ -15,66 +20,33 @@ uint8_t bpm = 90;
 unsigned int initialDelay = 1000;
 unsigned long ellapsedTime, initTime, lastResync;
 
-bool printOutput = true;
 bool variableBpm = true;
-bool hasStarted = false;
 
 short minBpm = 60;
 short maxBpm = 150;
 
-int lastButtonState = LOW;
-int currSpotlightMode = 0;
-int currMatrixMode = 0;
-
 bool robotIsPresent[NB_ROBOTS] = { true, false, false, true };
 
 void setup() {
-  if (printOutput) {
-    Serial.begin(9600);
-  }
-
-  pinMode(BPM_INPUT_PIN, INPUT);
-  pinMode(BUTTON1_PIN, INPUT);
+  MIDI.begin(MIDI_CHANNEL_OMNI);  // Initialize the Midi Library.
+  MIDI.setHandleNoteOn(handleNoteOn);
+  MIDI.setHandleNoteOff(handleNoteOff);
+  MIDI.setHandleControlChange(handleCCMessage);
 
   Wire.begin();
   //notifyRobots(SET_RESYNC_TIME, RESYNC_TIME);
+
+  pinMode(LED_PIN, OUTPUT);
 
   initTime = millis();
 }
 
 void loop() {
   ellapsedTime = millis() - initTime;
-  if (!hasStarted) {
-    if (ellapsedTime >= initialDelay) {
-      // We start robots
-      Serial.println("[RoboMaster] Start!");
-      notifyRobots(START);
-      lastResync = ellapsedTime;
-      hasStarted = true;
-    }
-  } else {
-    if (variableBpm) {
-      int sensorValue = analogRead(BPM_INPUT_PIN);
-      int newBpm = map(sensorValue, ANALOG_MIN, ANALOG_MAX, minBpm, maxBpm);
-      if (abs(newBpm - bpm) >= MIN_BPM_CHANGE) {
-        bpm = newBpm;
-        notifyRobots(BPM_CHANGE, bpm);
-      }
-    }
-    if (ellapsedTime - lastResync >= RESYNC_TIME) {
-      //notifyRobots(RESYNC);
-      lastResync += RESYNC_TIME;
-    }
-
-    int currButtonState = digitalRead(BUTTON1_PIN);
-    if (currButtonState == HIGH && lastButtonState == LOW) {
-      currMatrixMode = ++currMatrixMode % NB_MATRIX_MODES;
-      currSpotlightMode = ++currSpotlightMode % NB_SPOTLIGHT_MODES;
-
-      notifyChangeLightingMode(matrixModes[currMatrixMode], MATRIX);
-      notifyChangeLightingMode(spotlightModes[currSpotlightMode], SPOTLIGHT);
-    }
-    lastButtonState = currButtonState;
+  MIDI.read();
+  if (ellapsedTime - lastResync >= RESYNC_TIME) {
+    //notifyRobots(RESYNC);
+    lastResync += RESYNC_TIME;
   }
 }
 
@@ -125,4 +97,61 @@ void uint16ToArray(uint16_t inputNumber, uint8_t* arr) {
 void notifyChangeLightingMode(uint8_t lightingMode, uint8_t elementBit) {
   uint8_t messageContent = (lightingMode << BITS_PER_ELEMENT_IDENTIFIER) | elementBit;
   sendMessage(LIGHTING_ADDRESS, MODE_CHANGE, messageContent);
+}
+
+void handleNoteOn(byte channel, byte pitch, byte velocity) {
+  digitalWrite(LED_BUILTIN, HIGH);
+  switch (pitch) {
+    case C03:
+      notifyChangeLightingMode(MATRIX_OFF_MODE, MATRIX);
+      break;
+    case D03:
+      notifyChangeLightingMode(MATRIX_BLINKING_MODE, MATRIX);
+      break;
+    case E03:
+      notifyChangeLightingMode(MATRIX_NAME_MODE, MATRIX);
+      break;
+    case F03:
+      notifyChangeLightingMode(MATRIX_LOGO_MODE, MATRIX);
+      break;
+    case G03:
+      notifyChangeLightingMode(MATRIX_RECTANGLES_MODE, MATRIX);
+      break;
+    case A03:
+      notifyChangeLightingMode(MATRIX_BARS_MODE, MATRIX);
+      break;
+    case C04:
+      notifyChangeLightingMode(SPOTLIGHT_OFF_MODE, SPOTLIGHT);
+      break;
+    case D04:
+      notifyChangeLightingMode(SPOTLIGHT_BLINKING_MODE, SPOTLIGHT);
+      break;
+    case E04:
+      notifyChangeLightingMode(SPOTLIGHT_SEQUENCE_MODE, SPOTLIGHT);
+      break;
+    case F04:
+      notifyChangeLightingMode(SPOTLIGHT_CONSTANT_MODE, SPOTLIGHT);
+      break;
+    case C05:
+      notifyRobots(START);
+      break;
+  }
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity) {
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void handleCCMessage(byte channel, byte number, byte value) {
+  switch (number) {
+    case POT1:
+      if (variableBpm) {
+        int newBpm = map(value, CC_MIN, CC_MAX, minBpm, maxBpm);
+        if (abs(newBpm - bpm) >= MIN_BPM_CHANGE) {
+          bpm = newBpm;
+          notifyRobots(BPM_CHANGE, bpm);
+        }
+      }
+      break;
+  }
 }
