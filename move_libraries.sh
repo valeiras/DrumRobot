423797ccf0b2a1184efccdd931eaa23e0a85c96f
valeiras <d.r.valeiras@gmail.com>
2023-05-20 08:57:57 +0200

    Create move_libraries.sh

---
+++ move_libraries.sh
@@ -0,0 +1,11 @@
+rm -r ./libraries/PercuController
+rm -r ./libraries/PercuRobot
+rm -r ./libraries/PercuSong
+rm -r ./libraries/RoboCommunication
+rm -r ./libraries/RoboReceptor
+
+cp -r ~/Documents/Arduino/libraries/PercuController ./libraries
+cp -r ~/Documents/Arduino/libraries/PercuRobot ./libraries
+cp -r ~/Documents/Arduino/libraries/PercuSong ./libraries
+cp -r ~/Documents/Arduino/libraries/RoboCommunication ./libraries
+cp -r ~/Documents/Arduino/libraries/RoboReceptor ./libraries
